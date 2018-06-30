07879957c8d839c15e43c7872f30f0d0d535a472
Fábio Ferreira <martinhoferreira10@gmail.com>
2018-06-30 15:20:53 +0100

    Add menu component unit tests.

--- src/App/Menu/Menu.js
+++ src/App/Menu/Menu.js
@@ -1,66 +1,37 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 
-import './Menu.css';
-
-export class Menu extends Component {
-	constructor() {
-		super();
-		this.newGame = this.newGame.bind(this);
-		this.continueGame = this.continueGame.bind(this);
-	}
-
-	newGame() {
-		this.props.reload();
-	}
-
-	continueGame() {
-		this.props.continue();
-	}
-
-	render() {
-		return (
-			<div className="menu-container">
-				<div className="info">
-					{this.props.gameWon && <h1 id="perdeste">Won!!!</h1>}
-					{this.props.gameLost && <h1 id="ganhaste">Lost!!!</h1>}
-					<ul>
-						{this.props.gameStopped && !this.props.gameWon && !this.props.gameLost && <li id="continuar"><Button onClick={this.continueGame}>Continue</Button></li>}
-						<li id="novo_jogo" ><Button onClick={this.newGame}>New Game</Button></li>
-					</ul>
-				</div>
+const Menu = props =>
+	(
+		<div className="menu-container">
+			<div className="info">
+				{props.gameWon && <h1>Congratulations!!!</h1>}
+				{props.gameLost && <h1>Try again!!!</h1>}
+				<ul>
+					{props.gameStopped && !props.gameWon && !props.gameLost && <li><Button id="continue-game" onClick={props.continue}>Continue</Button></li>}
+					<li ><Button id="new-game" onClick={props.reload}>New Game</Button></li>
+				</ul>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+
 
 Menu.propTypes = {
 	gameWon: PropTypes.bool,
 	gameLost: PropTypes.bool,
 	gameStopped: PropTypes.bool,
-	reload: PropTypes.func.isRequired,
+	reload: PropTypes.func,
 	continue: PropTypes.func,
 };
 
 Menu.defaultProps = {
-	continue: () => {},
+	reload: () => {},
+	continue: () => { },
 	gameWon: false,
 	gameLost: false,
 	gameStopped: false,
 };
 
-const mapStateToProps = (state) => {
-	const gameWon = state.get('gameWon'),
-		gameLost = state.get('gameLost'),
-		gameStopped = state.get('gameStopped');
-
-	return {
-		gameWon,
-		gameLost,
-		gameStopped,
-	};
-};
-
-export default connect(mapStateToProps)(Menu);
+export default Menu;
--- src/App/Menu/__tests__/Menu.test.js
+++ src/App/Menu/__tests__/Menu.test.js
@@ -2,16 +2,70 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import { Menu } from '../Menu';
+import Menu from '../Menu';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Menu component', () => {
-	it('Menu should new game button', () => {
+	it('should show new game button that on click should call reload parameter function', () => {
 		const newGame = jest.fn(),
 			menu = shallow(<Menu reload={newGame} />),
-			buttons = menu.find('#novo_jogo');
+			newGameButton = menu.find('#new-game');
 
-		expect(buttons).toHaveLength(1);
+		expect(newGameButton).toHaveLength(1);
+
+		newGameButton.simulate('click');
+
+		expect(newGame).toHaveBeenCalledTimes(1);
+	});
+
+	it('should show continue game button if game is stopped and it is not finished and on click call continue property function', () => {
+		const continueGame = jest.fn();
+
+		let menu = shallow(<Menu gameStopped continue={continueGame} />),
+			continueGameButton = menu.find('#continue-game');
+		expect(continueGameButton).toHaveLength(1);
+		continueGameButton.simulate('click');
+		expect(continueGame).toHaveBeenCalledTimes(1);
+
+		menu = shallow(<Menu gameStopped gameWon />);
+		continueGameButton = menu.find('#continue-game');
+		expect(continueGameButton).toHaveLength(0);
+
+		menu = shallow(<Menu gameStopped gameLost />);
+		continueGameButton = menu.find('#continue-game');
+		expect(continueGameButton).toHaveLength(0);
+	});
+
+	it('should text game won if prop is passed', () => {
+		const newGame = jest.fn(),
+			menu = shallow(<Menu reload={newGame} gameWon />),
+			textWon = menu.find('h1[children="Congratulations!!!"]');
+
+		expect(textWon).toHaveLength(1);
+	});
+
+	it('should text game won if prop is false', () => {
+		const newGame = jest.fn(),
+			menu = shallow(<Menu reload={newGame} gameWon={false} />),
+			textWon = menu.find('h1[children="Congratulations!!!"]');
+
+		expect(textWon).toHaveLength(0);
+	});
+
+	it('should text game lost if prop is passed', () => {
+		const newGame = jest.fn(),
+			menu = shallow(<Menu reload={newGame} gameLost />),
+			textLost = menu.find('h1[children="Try again!!!"]');
+
+		expect(textLost).toHaveLength(1);
+	});
+
+	it('should text game lost if prop is false', () => {
+		const newGame = jest.fn(),
+			menu = shallow(<Menu reload={newGame} gameLost={false} />),
+			textLost = menu.find('h1[children="Try again!!!"]');
+
+		expect(textLost).toHaveLength(0);
 	});
 });
